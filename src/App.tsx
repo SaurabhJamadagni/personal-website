ed01771cca4d5909078a6c03ccd7f69cfc79b0ef
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2023-05-25 17:01:49 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Name from './Name.tsx'
+
+function App() {
+  return <div>
+    <Name />
+  </div>;
+}
+
+export default App;
---
+++ src/Name.tsx
@@ -0,0 +1,5 @@
+function Name() {
+    return <h1>saurabh jamadagni</h1>;
+}
+
+export default Name;
